e79b7eeaf072ec9736bf40033d31207ec11dc0ad
Mark <113670055+mrvbltzr@users.noreply.github.com>
2025-02-17 15:39:29 +0800

    Rename .env to .metadata
    "env" does not make any sense.
    "metadata" does.

--- _resources/templater/src/env.ts
+++ _resources/templater/src/env.ts
@@ -2,18 +2,23 @@ import dotenv from 'dotenv';
 import { dirname, join } from 'path';
 import { readFile } from 'fs/promises';
 
-export interface Templater {
+interface Templater {
     file: {
         path: (absolute?: boolean) => string;
     };
 }
 
+interface EnvKeys extends dotenv.DotenvParseOutput {
+    COURSE_TITLE: string;
+    COURSE_URL: string;
+}
+
 /**
  * Access the .env file.
  */
-export default async (tp: Templater, key?: string) => {
-    const envContent = await readFile(join(dirname(tp.file.path()), '.env'));
-    const envs = dotenv.parse(envContent);
+export default async (tp: Templater, key?: keyof EnvKeys) => {
+    const envContent = await readFile(join(dirname(tp.file.path()), '.metadata'));
+    const envs = dotenv.parse<EnvKeys>(envContent);
 
     return key ? envs[key] : envContent;
 };
